b1882fd2587d46de89fd787b0be83253c06e895f
arjun <arjunjhukal.makura@gmail.com>
2026-05-15 15:43:40 +0545

    minor changes

--- src/components/organism/Cards/TestCard.tsx
+++ src/components/organism/Cards/TestCard.tsx
@@ -106,14 +106,20 @@ export default function TestCard({ test, havePurchased, status: testStatus }: { 
               id={test?.course_id ? Number(test?.course_id) : Number(id)}
             />
             {testStatus === "completed" ? <div className="flex items-center gap-2">
-              <Donut
-                progress={test?.results?.score || 0}
-                size={60}
-                thickness={6}
-              />
+              {test?.test_type !== "subjective" && (
+                <Donut
+                  progress={test?.results?.score || 0}
+                  size={60}
+                  thickness={6}
+                />
+              )}
               <div className="content">
                 <strong className="block text-[12px] leading-1">Your Score</strong>
-                <p className="text-[14px]"><strong>{test?.results?.attempted || 0}</strong>/{test?.total_questions}</p>
+                {test?.test_type === "subjective" ? (
+                  <p className="text-[14px] flex items-center"><Typography variant="h4" fontWeight={600} color="primary">{test?.results?.score || 0}</Typography>/{test?.full_mark}</p>
+                ) : (
+                  <p className="text-[14px]"><strong>{test?.results?.attempted || 0}</strong>/{test?.total_questions}</p>
+                )}
               </div>
             </div> : ""}
           </div>
